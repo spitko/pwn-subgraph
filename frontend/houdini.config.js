80dbf32a8bb48c2745ae2adac79e948d1675152e
Samuel Pitko <samuel.pitko@gmail.com>
2024-06-01 20:15:20 +0200

    houdini init

---
+++ frontend/houdini.config.js
@@ -0,0 +1,13 @@
+/// <references types="houdini-svelte">
+
+/** @type {import('houdini').ConfigFile} */
+const config = {
+    "watchSchema": {
+        "url": "https://api.studio.thegraph.com/query/25077/pwn/version/latest"
+    },
+    "plugins": {
+        "houdini-svelte": {}
+    }
+}
+
+export default config
